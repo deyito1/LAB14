2728219e8a2e92f02ed8a553cf6b402bf74815c3
deyito1 <space123580@gmail.com>
2024-11-18 11:22:39 -0500

    PRIMERA PARTE

---
+++ app/src/main/java/com/tecsup/lab14/SimpleWidget.kt
@@ -0,0 +1,8 @@
+package com.tecsup.lab14
+
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class SimpleWidget : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = SimpleWidgetContent()
+}
